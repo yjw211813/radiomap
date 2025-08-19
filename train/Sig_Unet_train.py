9cd9cfbd36ba9afa91db63a4bac24777cdd215ac
liaozhengyan <liaozhengyan1999@163.com>
2025-08-19 18:58:35 +0800

    将插值作为模型输入

--- train/Sig_Unet_train.py
+++ train/Sig_Unet_train.py
@@ -47,7 +47,7 @@ attn_params = [C_list_attn * 2, C_list_attn , C_list_attn // 2, C_list_attn // 2
 log_dir = r'/home/code/radio_map_construction/runs/model_log/BTM_multi_scale_v6'# log 存储位置
 model_load_dir = "/home/code/radio_map_construction/runs/model_pth/BTM_multi_scale_v6/"# 模型加载目录
 model_save_dir = "/home/code/radio_map_construction/runs/model_pth/BTM_multi_scale_v6/"# 模型存储位置
-device = torch.device('cuda:3' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
 
 
 def batch_sample_images_torch(input_tensor, samples_per_image=4, fix_samples=0,
